a10bc0e5046eef800645b606eb1a57a5849388c7
Karl August Hoivik <gussefant@gmail.com>
2017-02-22 01:54:41 +0100

    Vaktliste

--- infoscreen/views.py
+++ infoscreen/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from userprofile.models import Profile
 
 
 def infoscreen(request):
-    return render(request, 'infoscreen.html', {})
+    vakter = Profile.objects.all()[:2];
+    return render(request, 'infoscreen.html', {'vakter': vakter})
