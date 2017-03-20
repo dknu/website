317119ce89ca29b914a320188aaeae0555b8e848
Sigmund Eggen Holm <Sigmund@Sigmunds-Mac.lan>
2017-03-20 20:14:51 +0100

    Links in header, register button on login page.

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from authentication import views
 
+app_name = 'authentication'
+
 urlpatterns = [
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
