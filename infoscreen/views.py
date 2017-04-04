09f771bd5b0090ee8c8eca07957795b174c7d668
anniksan <sanniken89@gmail.com>
2017-04-04 19:50:11 +0200

    Copypaste stuff from /rpi

--- infoscreen/views.py
+++ infoscreen/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+
+from rpi.models import RaspberryPi
 from userprofile.models import Profile
 
 
 def infoscreen(request):
     on_duty = Profile.objects.all()[:2];
-    return render(request, 'infoscreen.html', {'on_duty': on_duty})
-
-
+    rpis = RaspberryPi.objects.all()
+    return render(request, 'infoscreen.html', {
+        'on_duty': on_duty,
+        'rpis': rpis
+    })
