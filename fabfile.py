4d464bd92347a75705fecbdf04e9775982b2fc72
ecuatox <a.tenstad@gmail.com>
2017-05-05 12:38:41 +0200

    Allow all projects

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,30 @@
-from fabric.api import env, run, cd
+from fabric.api import env, run, cd, abort
 from fabric.context_managers import prefix
 from fabric.operations import sudo
 
 env.hosts = ['hackerspace-ntnu.no']
 root_folder = '/devops/'
 project_folder = '/website'
+projects = ['prod', 'dev']
 
 
-def deploy(project='prod', branch='master'):
+def handle_project(func):
+    def _handle_project(*args, **kwargs):
+        if not args:
+            func(projects[0], *args[1:], **kwargs)
+        elif args[0] == 'all':
+            for project in projects:
+                func(project, *args[1:], **kwargs)
+        elif args[0] in projects:
+            func(args[0], *args[1:], **kwargs)
+        else:
+            abort('Invalid project')
+
+    return _handle_project
+
+
+@handle_project
+def deploy(project, branch='master'):
     git(project, branch)
     requirements(project)
     django(project)
@@ -15,7 +32,8 @@ def deploy(project='prod', branch='master'):
     nginx()
 
 
-def git(project='prod', branch='master'):
+@handle_project
+def git(project, branch='master'):
     with cd(root_folder + project + project_folder):
         sudo('git stash', user='git')
         sudo('git fetch --all', user='git')
@@ -23,20 +41,23 @@ def git(project='prod', branch='master'):
         sudo('git reset --hard origin/%s' % branch, user='git')
 
 
-def requirements(project='prod'):
+@handle_project
+def requirements(project):
     with cd(root_folder + project + project_folder), prefix('. ../%senv/bin/activate' % project):
         run('pip install -r requirements.txt')
         run('pip install -r prod_requirements.txt')
 
 
-def django(project='prod'):
+@handle_project
+def django(project):
     with cd(root_folder + project + project_folder), prefix('. ../%senv/bin/activate' % project):
         run('python manage.py makemigrations')
         run('python manage.py migrate')
         run('python manage.py collectstatic --no-input')
 
 
-def gunicorn(project='prod'):
+@handle_project
+def gunicorn(project):
     sudo('systemctl restart %s' % project)
 
 
@@ -44,6 +65,7 @@ def nginx():
     sudo('systemctl restart nginx')
 
 
-def restart(project='prod'):
+@handle_project
+def restart(project):
     gunicorn(project)
     nginx()
