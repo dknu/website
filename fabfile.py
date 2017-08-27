6a8054213c82ca4436e3add01ba59b8e70d1dd89
Amund Tenstad <github@amund.io>
2017-08-27 19:40:05 +0200

    Fix docker install

--- fabfile.py
+++ fabfile.py
@@ -26,8 +26,9 @@ def init():
 ######################
 
 def install_docker():
-    sudo('sudo curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose')
-    sudo('sudo chmod +x /usr/local/bin/docker-compose')
+    sudo('apt-get install docker.io')
+    sudo('curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose')
+    sudo('chmod +x /usr/local/bin/docker-compose')
 
 
 ######################
