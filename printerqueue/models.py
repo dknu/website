85683ec73a2a1b30b5a58048b2ea9494309761c0
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2017-11-13 10:51:55 +0100

    Connect app to base template

--- printerqueue/models.py
+++ printerqueue/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ForeignKey, DateTimeField, timezone, CharField, TextField
 
-# Create your models here.
+
+class Queue(models.Model):
+    name = CharField(max_length=128)
+    description = TextField()
+
+    def __str__(self):
+        return str(self.name)
+
+class QueueObject(models.Model):
+    user = ForeignKey(User)
+    submitted_date = DateTimeField(default=timezone.now)
+    description = CharField(max_length=64)
+
--- printerqueue/views.py
+++ printerqueue/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def landing(request):
     context = {"debug":"Debug message"}
     return render(request, 'printerqueue/landing.html', context)
