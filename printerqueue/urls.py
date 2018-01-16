9a7d2f72cf17bf883046fd04d2a7e39d967ee44a
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-01-16 23:14:42 +0100

    Refactored urls

--- printerqueue/urls.py
+++ printerqueue/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from printerqueue import views
 
@@ -8,8 +8,10 @@ urlpatterns=[
     url(r'^$', views.index, name="queue"),
     url(r'^all$', views.all_queues, name="all"),
     url(r'^create$', views.create, name="create"),
-    url(r'^(?P<queue_id>.+)/view$', views.show, name="view"),
-    url(r'^(?P<queue_id>.+)/add$', views.add_to_queue, name="add_to_queue"),
+    url(r'^(?P<queue_id>.+)/', include([
+        url(r'^view$', views.show, name="view"),
+        url(r'^add$', views.add_to_queue, name="add")
+    ])),
     url(r'^gettimes/(?P<queue_id>[0-9]+)/(?P<date>.*)$', views.get_times, name="get_times"),
     url(r'^getendtimes/(?P<queue_id>[0-9]+)/(?P<date>.*)/(?P<time>.*)$', views.get_end_times, name="get_end_times"),
 
