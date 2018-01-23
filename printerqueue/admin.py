e9048d4a475abdfc8b070fed059047e9ef3840e1
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-01-23 19:38:31 +0100

    Fixed registering of Queue and QueueObject to admin panel

--- printerqueue/admin.py
+++ printerqueue/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from printerqueue.models import Queue, QueueObject
 
-admin.register(Queue)
-admin.register(QueueObject)
+admin.site.register(Queue)
+admin.site.register(QueueObject)
 
